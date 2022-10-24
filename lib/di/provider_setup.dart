40ac019d13fd9df254a314dbd2a96394f0884b99
d0ngwooK1m <dongwookim.dev@gmail.com>
2022-10-24 14:23:57 +0900

    provider_setup 추가

---
+++ lib/di/provider_setup.dart
@@ -0,0 +1,27 @@
+import 'package:hive/hive.dart';
+import 'package:kotori/data/repository/diary_repository_impl.dart';
+import 'package:kotori/data/repository/item_repository_impl.dart';
+import 'package:kotori/data/source/diary/diary_dao_impl.dart';
+import 'package:kotori/data/source/diary/diary_entity.dart';
+import 'package:kotori/data/source/item/item_dao_impl.dart';
+import 'package:kotori/data/source/item/item_entity.dart';
+import 'package:kotori/presentation/diary/diary_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+Future<List<SingleChildWidget>> getProviders() async {
+  final diaryBox = await Hive.openBox<DiaryEntity>('diary.db');
+  final itemBox = await Hive.openBox<ItemEntity>('item.db');
+
+  final diaryDao = DiaryDaoImpl(diaryBox);
+  final itemDao = ItemDaoImpl(itemBox);
+
+  final diaryRepository = DiaryRepositoryImpl(diaryDao);
+  final itemRepository = ItemRepositoryImpl(itemDao);
+
+  final diaryViewModel = DiaryViewModel(diaryRepository);
+
+  return [
+    ChangeNotifierProvider(create: (_) => diaryViewModel),
+  ];
+}
